8a520c2da85664741d88837b90fd4364e59b35d7
Joseph Goldin <jgoldin@skillz.com>
2020-09-30 14:44:05 -0700

    Added quick chat, updated `server_sdk.jar`

--- example_sync_server/src/com/skillz/example_sync_server/Main.groovy
+++ example_sync_server/src/com/skillz/example_sync_server/Main.groovy
@@ -1,10 +1,9 @@
 package com.skillz.example_sync_server
 
-import com.skillz.server.MessageHandler
 import com.skillz.server.Server
 
 class Main {
     static void main(final String[] args) {
-        Server.start(MessageHandler.Type.TICK)
+        Server.start()
     }
 }
